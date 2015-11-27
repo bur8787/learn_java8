126ab24da2c05739bc92975e21f52137ff88e819
Takahiro Suzuki <bur8787@gmail.com>
2015-11-28 01:42:16 +0900

    Add Sample1_3.java

---
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_3.java
@@ -0,0 +1,13 @@
+package jp.suzutt.learn_java8;
+
+public class Sample1_3 {
+	static Integer num;
+	public static void main(String[] args) {
+		switch(num){
+			case 0:
+				System.out.println(0);	break;
+			case 1:
+				System.out.println(1);	break;
+		}
+	}
+}
