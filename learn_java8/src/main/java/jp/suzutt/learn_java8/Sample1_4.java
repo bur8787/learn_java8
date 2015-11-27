e60c4c8506d07d402f16bdf52116d6794aa0421a
Takahiro Suzuki <bur8787@gmail.com>
2015-11-28 01:50:18 +0900

    Add Sample1_4.java

---
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_4.java
@@ -0,0 +1,17 @@
+package jp.suzutt.learn_java8;
+
+public class Sample1_4 {
+	public static void main(String[] args) {
+		String signal = args[0];
+		switch(signal){
+			case "red":
+				System.out.println("赤");	break;
+			case "yellow":
+				System.out.println("黄");	break;
+			case "blue":
+				System.out.println("青");	break;
+			default:
+				System.out.println("その他");	break;
+		}
+	}
+}
