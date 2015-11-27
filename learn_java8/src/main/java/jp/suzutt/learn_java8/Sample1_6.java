3bc3379f15c837040d92e45240b164790bc8927c
Takahiro Suzuki <bur8787@gmail.com>
2015-11-28 02:14:20 +0900

    Add Sample1_6.java

---
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_6.java
@@ -0,0 +1,17 @@
+package jp.suzutt.learn_java8;
+
+public class Sample1_6 {
+	public static void main(String[] args) {
+//		float x1 = 3_.1415F;
+//		float x2 = 3._1415F;
+//		long x3 = 999_99_99999_L;
+//		int x4 = _52;
+		int x5 = 5_2;
+//		int x6 = 52_;
+		int x7 = 5_______2;
+//		int x8 = 0_x52;
+//		int x9 = 0x_52;
+		int x10 = 0x5_2;
+		int x11 = 0_52;
+	}
+}
