83a74c19e08c970816cccf7ae1a67b5d6ce8b09d
Takahiro Suzuki <bur8787@gmail.com>
2015-11-28 02:00:04 +0900

    Add Sample1_5.java

---
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_5.java
@@ -0,0 +1,15 @@
+package jp.suzutt.learn_java8;
+
+public class Sample1_5 {
+	public static void main(String[] args) {
+		int val1 = 26;	//10進数
+		int val2 = 032;	//8進数
+		int val3 = 0x1a;	//16進数
+		int val4 = 0b11010;	//2進数
+//		int val5 = 0b12010; //コンパイルエラー
+		System.out.println("val1 :" + val1);
+		System.out.println("val2 :" + val2);
+		System.out.println("val3 :" + val3);
+		System.out.println("val4 :" + val4);
+	}
+}
