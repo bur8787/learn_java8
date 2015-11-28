f62aa594cd1383f902be44bd9eb742d72149bbaf
Takahiro Suzuki <bur8787@gmail.com>
2015-11-29 01:23:02 +0900

    Modify Sample1_9.java

--- learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_9.java
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_9.java
@@ -8,4 +8,8 @@ public class Sample1_9 {
 	int methodB() { return staticVal; }
 //	static int methodC() { return instanceVal;}
 	static int methodD() { return staticVal;}
+	static int methodE() {
+		Sample1_9 obj = new Sample1_9();
+		return obj.instanceVal;
+	}
 }
