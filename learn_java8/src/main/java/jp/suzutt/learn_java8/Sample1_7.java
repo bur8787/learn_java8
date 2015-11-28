e980ad6b0ea98e8b6fdbb5541d32fcb7451989c7
Takahiro Suzuki <bur8787@gmail.com>
2015-11-28 23:45:37 +0900

    Add Sample1_7.java

---
+++ learn_java8/src/main/java/jp/suzutt/learn_java8/Sample1_7.java
@@ -0,0 +1,11 @@
+package jp.suzutt.learn_java8;
+
+class SuperA {}
+final class SuperB {}
+class SuperC { void print(){} }
+class SuperD { final void print(){} }
+
+class SubA extends SuperA {}
+//class SubB extends SuperB {}
+class SubC extends SuperC { void print(){} }
+//class SubD extends SuperD { void print(){} }
